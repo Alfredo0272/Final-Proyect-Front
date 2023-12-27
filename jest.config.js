cf99abe53653bcca9eab3beb09678e8ee12fb32f
Alfredo <alfloz01@ucm.es>
2023-12-27 11:00:32 +0100

    Initial Commit

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['dist'],
+  resolver: 'jest-ts-webcompat-resolver',
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  coveragePathIgnorePatterns: [
+    'src/main.tsx',
+    'src/config.tsx',
+    'src/services/storage.tsx',
+    'src/store/store.tsx',
+    'src/pages/*.ts',
+    'src/services/images.ts',
+    'src/entities/*.ts',
+    'src/types/*.tsx',
+  ],
+};
