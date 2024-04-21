69b223da05580529c3b9ebaecb96e074439ffc0a
Alfredo <alfloz01@ucm.es>
2024-04-21 11:56:14 +0200

    Correct route for Render server only

--- src/config.tsx
+++ src/config.tsx
@@ -1,4 +1,1 @@
-export const serverUrl =
-  location.hostname === 'http://localhost:1969'
-    ? 'https://final-proyect-alfredo.onrender.com'
-    : 'http://localhost:1969';
+export const serverUrl = 'https://final-proyect-alfredo.onrender.com';
