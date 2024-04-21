9923bf064814a4df7e3633bc7f74a907f27ee423
Alfredo <alfloz01@ucm.es>
2024-04-21 11:21:25 +0200

    Create a favicon and link de config to the render server

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,4 @@
-// Export const serverUrl = location.hostname === 'http://localhost:1969';
-// // ? 'https://final-proyect-alfredo.onrender.com'
-// // : 'http://localhost:1969';
-
-export const serverUrl = 'http://localhost:1969';
+export const serverUrl =
+  location.hostname === 'http://localhost:1969'
+    ? 'https://final-proyect-alfredo.onrender.com'
+    : 'http://localhost:1969';
